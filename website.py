c2a90cf95076bae8582092c411a447ff654c419c
Arthur Serge <66523461+Sergearthur1@users.noreply.github.com>
2024-08-04 14:48:38 +0200

    Update website.py

--- website.py
+++ website.py
@@ -144,7 +144,7 @@ else:
         selected_rows.append(selected_tamp)
     data = st.session_state["full_data"][selected_rows]
 st.dataframe(
-    data.style.applymap(lambda x: 'background-color : grey'),
+    data.style.applymap(lambda x: 'background-color : green'),
     hide_index=True,
     width=5000,
     height=700,
